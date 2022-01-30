8894e2e8546f82960b7aceb5b0344870a7d8d0b5
ajmalmuhammed <37176892+ajmalmuhammed@users.noreply.github.com>
2022-01-30 20:41:55 +0530

    Time: 1 ms (39.71%), Space: 41.1 MB (5.29%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -2,13 +2,14 @@ class Solution {
     public int fib(int n) {
         //tabulation method
         int fibNums[] = new int[n+1];
-        fibNums[0]=0;
-        if(n>0){
-            fibNums[1]=1;
-            for(int i=2; i<=n; i++){
-                fibNums[i] = fibNums[i-1]+fibNums[i-2];
-            }
+        int curr=0,prev1=1,prev2=0;
+        if(n==0) return 0;
+        for(int i=2; i<=n; i++){
+            curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return fibNums[n];
+        
+        return prev1;
     }
 }
