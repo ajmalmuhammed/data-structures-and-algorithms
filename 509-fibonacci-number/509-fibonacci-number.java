c1f488719465138009a44f5a2d57eba32c385eed
ajmalmuhammed <37176892+ajmalmuhammed@users.noreply.github.com>
2022-01-30 20:44:18 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (5.29%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -3,7 +3,7 @@ class Solution {
         //tabulation method
         int curr=0,prev1=1,prev2=0;
         if(n==0) return 0;
-        for(int i=2; i<=n; i++){
+        for(int i=2; i<=n; ++i){
             curr = prev1+prev2;
             prev2 = prev1;
             prev1 = curr;
